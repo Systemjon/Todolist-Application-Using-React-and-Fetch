63e9cc522ec3afaac624e340b85cf2ec841896a0
joan <joanflores905@gmail.com>
2022-08-31 22:58:57 +0000

    version todo pero falta counter y mejorar con css

---
+++ src/js/component/App.jsx
@@ -0,0 +1,58 @@
+//import React from "react";
+import React, { useState } from 'react';
+//include images into your bundle
+
+
+//create your first component
+export function App () {
+	const [list, setList]= useState([]);
+	const [input, setInput]= useState("");
+
+    const addTodo = (todo) => {
+        const newTodo = {
+            id: Math.random(),
+            todo: todo,
+        };
+
+        //agregar todo a la lista 
+        setList([...list, newTodo])
+
+        //clear input box
+        setInput("")
+    };
+
+    const deleteTodo = (id) => {
+        const newList = list.filter((todo) => todo.id !== id);
+        setList(newList);
+    }
+
+	return (
+		<div className="text-center">
+			<h1 className="Todo">todos</h1>
+			<div className="file-input">
+			<input
+				type="text"
+            	className="text"
+            	value={input}
+            	onChange={e => setInput(e.target.value)}
+                onKeyDown={(e) => {
+                    if (e.key == "Enter") {
+                        addTodo(input)
+                    }
+                }}
+			/>
+			
+            <ul className="list-group-item">
+                {list.map((todo) => (
+                    <li className="separar list-group-item" key={todo.id}>
+                        <span>{todo.todo}</span>
+                        <button className='boton btn btn-light me-0' onClick={() => deleteTodo(todo.id)}>&times;</button>
+                    </li>
+                ))}
+            </ul>
+			</div>
+		</div>
+
+	);
+};
+export default App;
